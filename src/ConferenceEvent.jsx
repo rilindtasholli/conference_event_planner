b8d76e7d0be6d3fbab78cca7da7bdbc494fa0b3f
Rilind Tasholli <rilindtasholli@gmail.com>
2026-09-11 15:46:49 +0200

    add total cost and small fix

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -60,6 +60,29 @@ const ConferenceEvent = () => {
 
   const getItemsFromTotalCost = () => {
     const items = [];
+    venueItems.forEach((item) => {
+      if (item.quantity > 0) {
+        items.push({ ...item, type: "venue" });
+      }
+    });
+    avItems.forEach((item) => {
+      if (
+        item.quantity > 0 &&
+        !items.some((i) => i.name === item.name && i.type === "av")
+      ) {
+        items.push({ ...item, type: "av" });
+      }
+    });
+    mealsItems.forEach((item) => {
+      if (item.selected) {
+        const itemForDisplay = { ...item, type: "meals" };
+        if (item.numberOfPeople) {
+          itemForDisplay.numberOfPeople = numberOfPeople;
+        }
+        items.push(itemForDisplay);
+      }
+    });
+    return items;
   };
 
   const items = getItemsFromTotalCost();
@@ -140,33 +163,6 @@ const ConferenceEvent = () => {
         setShowItems(!showItems); // Toggle showItems to true only if it's currently false
       }
     }
-  };
-
-  const getItemsFromTotalCost = () => {
-    const items = [];
-    venueItems.forEach((item) => {
-      if (item.quantity > 0) {
-        items.push({ ...item, type: "venue" });
-      }
-    });
-    avItems.forEach((item) => {
-      if (
-        item.quantity > 0 &&
-        !items.some((i) => i.name === item.name && i.type === "av")
-      ) {
-        items.push({ ...item, type: "av" });
-      }
-    });
-    mealsItems.forEach((item) => {
-      if (item.selected) {
-        const itemForDisplay = { ...item, type: "meals" };
-        if (item.numberOfPeople) {
-          itemForDisplay.numberOfPeople = numberOfPeople;
-        }
-        items.push(itemForDisplay);
-      }
-    });
-    return items;
   };
 
   return (
